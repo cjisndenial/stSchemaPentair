3fa1ff8cf74ff2a8c7c05ecc72de1bd9d129ccaa
CJisNdenial <cj@ndenial.com>
2020-05-23 08:34:06 -0500

    Base Oauth & ST Responses function now

--- src/stSchema/index.js
+++ src/stSchema/index.js
@@ -3,44 +3,47 @@
 const { lambda } = require("st-schema");
 
 async function discoveryRequest(request, response) {
-    console.log(JSON.stringify(request,null,2))
+    console.log(JSON.stringify(request));
     response.addDevice('3rd-party-device-0001', 'Kitchen Light', 'c2c-color-temperature-bulb')
         .manufacturerName('Your Company')
         .modelName("Model A Tunable White Bulb")
         .hwVersion('v1 US Tunable White Bulb')
         .swVersion('1.0.0')
         .roomName('Kitchen')
-        .addGroup('Kitchen Table Lights')
+        .addGroup('Kitchen Table Lights');
+    console.log(response);
 }
 
 async function stateRefreshRequest(request, response) {
-    console.log(JSON.stringify(request,null,2))
+    console.log(JSON.stringify(request));
     const device = response.addDevice('3rd-party-device-0001');
     const component = device.addComponent('main');
-    component.addState('switch', 'switch', 'off');
-    component.addState('switchLevel', 'level', 80);
-    component.addState('colorTemperature', 'colorTemperature', 3000);
+    component.addState('st.switch', 'switch', 'off');
+    component.addState('st.switchLevel', 'level', 80);
+    component.addState('st.colorTemperature', 'colorTemperature', 3000);
+    console.log(response);
 }
 
 async function commandRequest(request, response) {
-    console.log(JSON.stringify(request,null,2))
+    console.log(JSON.stringify(request));
     request.devices.forEach(async (it) => {
         const device = response.addDevice(it.externalDeviceId);
         const component = device.addComponent("main");
         it.commands.forEach(async (command) => {
             switch(command.capability) {
                 case 'st.switch':
-                    component.addState('switch', 'switch', command.command);
+                    component.addState('st.switch', 'switch', command.command);
                     break;
                 case 'st.switchLevel':
-                    component.addState('switchLevel', 'level', command.arguments[0]);
+                    component.addState('st.switchLevel', 'level', command.arguments[0]);
                     break;
                 case 'st.colorTemperature':
-                    component.addState('colorTemperature', 'colorTemperature', command.arguments[0]);
+                    component.addState('st.colorTemperature', 'colorTemperature', command.arguments[0]);
                     break;
             }
         });
     });
+    console.log(response);
 }
 
 module.exports.handler = lambda({
--- src/stSchemaProxy/index.js
+++ src/stSchemaProxy/index.js
@@ -1,6 +1,28 @@
+const AWS = require('aws-sdk');
+
+const lambda = new AWS.Lambda();
+
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+  console.log(JSON.stringify(event));
+  
+  // To Do?  check auth token here?  Maybe harder inside the ST library?  Will have to see.
+ 
+  // Pass the stSchema request out of the HttpApi event body to the stSchema lambda function.  
+  const params = {
+    FunctionName: process.env.FUNCTION_NAME,
+    InvocationType: "RequestResponse",
+    Payload: event.body
+  };
 
-  return {};
+  console.log(params);
+
+  try {
+    const result = await lambda.invoke(params).promise();
+    console.log(result);
+    return result.Payload;
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    return new Error(`Error calling stSchema: ${JSON.stringify(error)}`);
+  }
 };
